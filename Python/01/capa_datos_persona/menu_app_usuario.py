1e0fdeb2e9c8c5913f26a07ba7221094a31cb303
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-08-18 10:59:52 -0300

    1.5 Comenzamos las pruebas de la Clase UsuarioDAO y menu_app_usuario

---
+++ Python/01/capa_datos_persona/menu_app_usuario.py
@@ -0,0 +1,9 @@
+opcion = None
+while opcion != 5:
+    print("Opciones: ")
+    print("1. Listar")
+    print("2. Actualizar")
+    print("3. Modificar")
+    print("4. Eliminar")
+    print("5. Salir")
+    opcion = int(input("Ingrese una opción (1-5): "))
