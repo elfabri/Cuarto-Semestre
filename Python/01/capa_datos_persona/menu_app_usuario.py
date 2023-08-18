48ec956b48ff4e9e8d7f3f83e032c227c65611a9
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-08-18 11:41:17 -0300

    1.6 Comenzamos a generar las diferentes opciones en menu_app_usuario - video 9

--- Python/01/capa_datos_persona/menu_app_usuario.py
+++ Python/01/capa_datos_persona/menu_app_usuario.py
@@ -20,9 +20,26 @@ while opcion != 5:
     elif opcion == 2:
         un = input(f"Ingrese nombre de usuario: ")
         pw = input(f"Ingrese contraseña: ")
+        
         usuario = Usuario(username=un, pw=pw)
         usu_insertado = UsuarioDAO.insertar(usuario)
         log.info(f"Usuario insertado: {usu_insertado}")
+
+    elif opcion == 3:
+        idu = int(input("Ingrese id de usuario: "))
+        un = input("Nombre: ")
+        pw = input("Contraseña: ")
+
+        usuario = Usuario(id_usuario=idu, username=un, pw=pw)
+        usu_actualizado = UsuarioDAO.actualizar(usuario)
+        log.info(f"Usuario actualizado: {usu_actualizado}")
+
+    elif opcion == 4:
+        idu = int(input("Ingrese id de usuario: "))
+
+        usuario = Usuario(id_usuario=idu)
+        usu_eliminado = UsuarioDAO.eliminar(usuario)
+        log.info(f"Usuario eliminado: {usu_eliminado}")
 
 else:
     log.info("Saliendo del programa")
