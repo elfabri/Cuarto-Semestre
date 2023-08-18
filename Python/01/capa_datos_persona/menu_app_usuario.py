b73a2c85e02350393c5c57697e47bbc4fb4b69cc
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-08-18 11:28:27 -0300

    1.6 Comenzamos a generar las diferentes opciones en menu_app_usuario - video 8

--- Python/01/capa_datos_persona/menu_app_usuario.py
+++ Python/01/capa_datos_persona/menu_app_usuario.py
@@ -1,11 +1,12 @@
 from usuario_dao import UsuarioDAO
+from Usuario import Usuario
 from logger_base import log
 
 opcion = None
 while opcion != 5:
     print("Opciones: ")
     print("1. Listar")
-    print("2. Actualizar")
+    print("2. Agregar")
     print("3. Modificar")
     print("4. Eliminar")
     print("5. Salir")
@@ -15,5 +16,13 @@ while opcion != 5:
         usuarios = UsuarioDAO.seleccionar()
         for u in usuarios:
             log.info(u)
+
+    elif opcion == 2:
+        un = input(f"Ingrese nombre de usuario: ")
+        pw = input(f"Ingrese contraseña: ")
+        usuario = Usuario(username=un, pw=pw)
+        usu_insertado = UsuarioDAO.insertar(usuario)
+        log.info(f"Usuario insertado: {usu_insertado}")
+
 else:
     log.info("Saliendo del programa")
