5a5e03c77f376b2cec19449005f5e80190dcab3b
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-08-19 15:26:11 -0300

    1.4 Método ejecutarOpciones() -> Parte 1 y 2

--- Java/01/estudiante/src/main/java/UTN/presentacion/SistemaEstudiantesApp.java
+++ Java/01/estudiante/src/main/java/UTN/presentacion/SistemaEstudiantesApp.java
@@ -39,13 +39,86 @@ public class SistemaEstudiantesApp {
     private static boolean ejecutarOp(Scanner consola, EstudianteDao estudianteDao) {
         var opcion = Integer.parseInt(consola.nextLine());
         var salir = false;
+
         switch (opcion) {
-            case 1 -> {
+            case 1 -> {  // Listar estudiantes
                 System.out.println("Listando estudiantes...");
                 var estudiantes = estudianteDao.listarEstudiantes();
                 estudiantes.forEach(System.out::println);
-                return salir;
+
+            } case 2 -> {  // Buscar estudiantes
+                System.out.println("Ingrese id del estudiante:");
+                var id = Integer.parseInt(consola.nextLine());
+                var est = new Estudiante(id);
+                var encontrado = estudianteDao.buscarEstPorId(est);
+
+                if (encontrado) {
+                    System.out.println("Estudiante encontrado!");
+                } else {
+                    System.out.println("Estudiante NO encontrado");
+                }
+            } case 3 -> {  // Agregar
+                System.out.println("Agregar estudiante");
+                System.out.println("Ingrese nombre:");
+                var nom = consola.nextLine();
+
+                System.out.println("Ingrese apellido:");
+                var ap = consola.nextLine();
+
+                System.out.println("Ingrese teléfono:");
+                var tel = consola.nextLine();
+
+                System.out.println("Ingrese email:");
+                var ema = consola.nextLine();
+
+                var est = new Estudiante(nom, ap, tel, ema);
+
+                var agre = estudianteDao.agregarEst(est);
+                if (agre) {
+                    System.out.println("Estudiante agregado: " + est);
+                } else {
+                    System.out.println("Estudiante NO agregado " + est);
+                }
+            } case 4 -> {
+                System.out.println("Modificar estudiante:");
+                System.out.println("Ingrese ID:");
+                var id = Integer.parseInt(consola.nextLine());
+
+                System.out.println("Ingrese nombre:");
+                var nom = consola.nextLine();
+
+                System.out.println("Ingrese apellido:");
+                var ap = consola.nextLine();
+
+                System.out.println("Ingrese teléfono:");
+                var tel = consola.nextLine();
+
+                System.out.println("Ingrese email:");
+                var ema = consola.nextLine();
+
+                var est = new Estudiante(id, nom, ap, tel, ema);
+
+                var mod = estudianteDao.agregarEst(est);
+                if (mod) {
+                    System.out.println("Estudiante modificado: " + est);
+                } else {
+                    System.out.println("Estudiante NO modificado " + est);
+                }
+
+            } case 5 -> {  // Eliminar
+                System.out.println("Modificar estudiante:");
+                System.out.println("Ingrese ID:");
+                var id = Integer.parseInt(consola.nextLine());
+                var est = new Estudiante(id);
+
+                var elim = estudianteDao.eliminarEstudiante(est);
+                if (elim) {
+                    System.out.println("Estudiante eliminado: " + est);
+                } else {
+                    System.out.println("Estudiante NO eliminado " + est);
+                }
             }
+            // case 3 -> {}
         }
     }
 }
