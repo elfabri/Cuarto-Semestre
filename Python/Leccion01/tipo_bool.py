f11bc28e23cddd505963b34fbb23a2cd9ef70b73
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-09-04 19:18:57 -0300

    1.2 Tipo Bool Parte 1 -> Trabajamos con el constructor bool()

---
+++ Python/Leccion01/tipo_bool.py
@@ -0,0 +1,23 @@
+# True or False
+# 0 -> false
+
+valor = 0.0
+resultado = bool(valor)
+print(f"valor: {valor}, resultado: {resultado}")
+
+
+valor = 0.1
+resultado = bool(valor)
+print(f"valor: {valor}, resultado: {resultado}")
+
+"""
+def uwu(val):
+    resul = bool(val)
+    print(f"valor: {val}, resultado: {resul}")
+
+uwu(0)
+uwu(15)
+uwu(-1)
+uwu((0,0))
+uwu(0.0)
+"""
