5e97942f1f097992e42dad137c9d72e9633eecbf
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-09-05 17:44:31 -0300

    2.3 Avanzamos con el modal del carrito en el header -> parte 2

--- JavaScript/e-commerce2022/client/js/cart.js
+++ JavaScript/e-commerce2022/client/js/cart.js
@@ -1,7 +1,13 @@
 const modalContainer = document.getElementById("modal-container");
 const modalOverlay = document.getElementById("modal-overlay");
 
+const cartBtn = document.getElementById("cart-btn");
+
 const displayCart = () => {
+    modalContainer.innerHTML = "";
+    modalContainer.style.display = "block";
+    modalOverlay.style.display = "block";
+
     // modal header
     const modalHeader = document.createElement("div");
 
@@ -10,4 +16,18 @@ const displayCart = () => {
     modalClose.className = "modal-close";
 
     modalHeader.append(modalClose);
+
+    modalClose.addEventListener("click", ()=>{
+        modalContainer.style.display = "none";
+        modalOverlay.style.display = "none";
+    })
+
+    const modalTitle = document.createElement("div");
+    modalTitle.innerText = "Cart";
+    modalTitle.className = "modal-title";
+    modalHeader.append(modalTitle);
+
+    modalContainer.append(modalHeader);
 }
+
+cartBtn.addEventListener("click", displayCart);
