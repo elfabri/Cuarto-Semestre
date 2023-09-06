d80058d79f5bd18c1c7825c0dd403a23468af962
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-09-06 14:25:36 -0300

    3.2 Ahora vamos al Footer

--- JavaScript/e-commerce2022/client/js/cart.js
+++ JavaScript/e-commerce2022/client/js/cart.js
@@ -50,6 +50,15 @@ const displayCart = () => {
         `;
         modalContainer.append(modalBody);
     })
+
+    // Modal footer
+    const modalFooter = document.createElement("div");
+    modalFooter.className = "modal-footer";
+    modalFooter.innerHTML = `
+    <div class="total-price">Total: </div>
+    `;
+    modalContainer.append(modalFooter);
+
 }
 
 cartBtn.addEventListener("click", displayCart);
