d8a822c163177e7bdefb7970b240e6d4ff5fbabe
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-09-06 13:56:45 -0300

    3.1 Avanzamos en el Body

--- JavaScript/e-commerce2022/client/js/index.js
+++ JavaScript/e-commerce2022/client/js/index.js
@@ -1,11 +1,12 @@
 const shopContent = document.getElementById("shopContent");
+const cart = [];
 
 productos.forEach((product) => {
     const content = document.createElement("div");
     content.innerHTML = `
     <img src="${product.img}">
     <h3>${product.productName}</h3>
-    <p>${product.price} $</p>
+    <p class="price">${product.price} $</p>
     `;
     shopContent.append(content);
 
