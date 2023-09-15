cfb998fc45dfb0fb6b55d5df00e8ccb9fc5e799a
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-09-15 14:45:24 -0300

    Js 4.2 Comenzamos con el método de pago - Parte 1

--- JavaScript/e-commerce2022/client/js/cart.js
+++ JavaScript/e-commerce2022/client/js/cart.js
@@ -31,62 +31,69 @@ const displayCart = () => {
     modalContainer.append(modalHeader);
 
     // Modal body
-    cart.forEach((p) => {
-        const mB = document.createElement("div");
-        mB.className = "modal-body";
-        mB.innerHTML = `
-        <div class="product">
-            <img class="product-img" src="${p.img}">
-            <div class="product-info">
-                <h4>${p.productName}</h4>
-            </div>
-            <div class="quantity">
-                <span class="quantity-btn-decrease">-</span>
-                <span class="quantity-input">${p.quantity}</span>
-                <span class="quantity-btn-increase">+</span>
-            </div>
-            <div class="price">$ ${p.price * p.quantity}</div>
-            <div class="delete-product">X</div>
-        </div>
-        `
-        modalContainer.append(mB);
+    if (cart.length > 0) {
+        cart.forEach((p) => {
+            const mB = document.createElement("div");
+            mB.className = "modal-body";
+            mB.innerHTML = `
+                <div class="product">
+                    <img class="product-img" src="${p.img}">
+                    <div class="product-info">
+                        <h4>${p.productName}</h4>
+                    </div>
+                    <div class="quantity">
+                        <span class="quantity-btn-decrease">-</span>
+                        <span class="quantity-input">${p.quantity}</span>
+                        <span class="quantity-btn-increase">+</span>
+                    </div>
+                    <div class="price">$ ${p.price * p.quantity}</div>
+                    <div class="delete-product">X</div>
+                </div>
+                `
+            modalContainer.append(mB);
 
-        // decrease product
-        const d = mB.querySelector(".quantity-btn-decrease");
-        d.addEventListener("click", () => {
-            if (p.quantity !== 1) {
-                p.quantity--;
+            // decrease product
+            const d = mB.querySelector(".quantity-btn-decrease");
+            d.addEventListener("click", () => {
+                if (p.quantity !== 1) {
+                    p.quantity--;
+                    displayCart();
+                }
+                displayCartCounter();
+            })
+
+            // increase product
+            const i = mB.querySelector(".quantity-btn-increase");
+            i.addEventListener("click", () => {
+                p.quantity++;
                 displayCart();
-            }
-            displayCartCounter();
+                displayCartCounter();
+            })
+
+            // delete product
+            const dP = mB.querySelector(".delete-product");
+            dP.addEventListener("click", () => {
+                deleteCartProduct(p.id);
+            })
         })
 
-        // increase product
-        const i = mB.querySelector(".quantity-btn-increase");
-        i.addEventListener("click", () => {
-            p.quantity++;
-            displayCart();
-            displayCartCounter();
-        })
+        // total
+        const total = cart.reduce((acc, el) => acc + el.price * el.quantity, 0);
 
-        // delete product
-        const dP = mB.querySelector(".delete-product");
-        dP.addEventListener("click", () => {
-            deleteCartProduct(p.id);
-        })
-    })
+        // Modal footer
+        const modalFooter = document.createElement("div");
+        modalFooter.className = "modal-footer";
+        modalFooter.innerHTML = `
+            <div class="total-price">Total: ${total}</div>
+            `;
+        modalContainer.append(modalFooter);
 
-    // total
-    const total = cart.reduce((acc, el) => acc + el.price * el.quantity, 0);
-
-    // Modal footer
-    const modalFooter = document.createElement("div");
-    modalFooter.className = "modal-footer";
-    modalFooter.innerHTML = `
-    <div class="total-price">Total: ${total}</div>
-    `;
-    modalContainer.append(modalFooter);
-
+    } else {
+        const modalText = document.createElement("H2");
+        modalText.className = "modal-body";
+        modalText.innerText = "cart is empty";
+        modalContainer.append(modalText);
+    }
 }
 
 cartBtn.addEventListener("click", displayCart);
