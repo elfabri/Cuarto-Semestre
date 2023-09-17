ddc1c83882622486fcbea12f4e6d4e32883fe8de
elfabri <Navarro.fa998@gmail.com>
2023-09-17 11:29:54 -0300

    Node C5 - 5.4 Ejecutamos para aclarar conceptos

--- Node/01-Hola-Mundo/monohilo.js
+++ Node/01-Hola-Mundo/monohilo.js
@@ -6,9 +6,11 @@ setInterval(function() {
     console.log(i);
     i++;
 
-    if (i === 5) {
-        console.log("forzamos un error");
-        const a = 3 + z;
-    }
+    // if (i === 5) {
+        // console.log("forzamos un error");
+        // const a = 3 + z;
+    // }
 
 }, 1000)
+
+console.log("Segunda instruccion");
