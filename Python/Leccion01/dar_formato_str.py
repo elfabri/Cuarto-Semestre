7b99c40535b05740f30966a9a15d3c18ba278389
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-09-27 10:35:42 -0300

    Python - 6.2 Avanzamos desde una tupla

--- Python/Leccion01/dar_formato_str.py
+++ Python/Leccion01/dar_formato_str.py
@@ -3,4 +3,10 @@
 nombre = "Adriel"
 edad = 28
 mensa_format = "Mi nombre es %s y tengo %d años" % (nombre, edad)
-print(mensa_format)
+
+# Creamos una tupla
+persona = ("Carla", "Gomez", 5000.00)
+mF = "Hola %s %s, tu sueldo es: %.2f"
+
+print(mF % persona)
+
