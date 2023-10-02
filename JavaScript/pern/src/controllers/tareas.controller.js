99941de78010cf503d983316355dfecbc320eac6
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-10-02 16:21:22 -0300

    JS - 6.2 Comenzamos con la conexión de crearTareas p1 a p4

--- JavaScript/pern/src/controllers/tareas.controller.js
+++ JavaScript/pern/src/controllers/tareas.controller.js
@@ -1,8 +1,25 @@
+import {pool} from "../db.js";
+
 export const listarTareas = (req, res) => res.send("Obteniendo tareas");
 
 export const listarTarea = (req, res) => res.send("Obteniendo tarea única");
 
-export const crearTarea = (req, res) => res.send("Creando tarea");
+export const crearTarea = async(req, res) => {
+    const { titulo, descripcion } = req.body;
+
+    try {
+        const resultado = await pool.query("INSERT INTO tareas (titulo, descripcion) VALUES ($1, $2) RETURNING *", [titulo, descripcion]);
+        res.json(resultado.rows[0]);
+        console.log(resultado.rows[0])
+    } catch (e) {
+
+        if (e.code === "23505") {
+            return res.send("Ya existe una tarea con ese titulo");
+        }
+        console.log(e);
+        next(e);
+    }
+};
 
 export const actualizarTareas = (req, res) => res.send("Actualizando tarea única");
 
--- JavaScript/pern/src/db.js
+++ JavaScript/pern/src/db.js
@@ -4,7 +4,8 @@ export const pool = new pg.Pool({
     port: 5432,
     host: "localhost",
     user: "postgres",
-    password: "admin"
+    password: "admin",
+    database: "PERN"
 });
 
 pool.on("connect", () => {
