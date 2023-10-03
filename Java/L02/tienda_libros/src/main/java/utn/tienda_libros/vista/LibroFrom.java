b78e620cd6c278aed8f667b841d199ff058272b5
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-10-03 14:08:08 -0300

    Java - 7.6 Constructor para la inyección de dependencias de Spring

--- Java/L02/tienda_libros/src/main/java/utn/tienda_libros/vista/LibroFrom.java
+++ Java/L02/tienda_libros/src/main/java/utn/tienda_libros/vista/LibroFrom.java
@@ -1,7 +1,25 @@
 package utn.tienda_libros.vista;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import utn.tienda_libros.servicio.LibroServicio;
+
 import javax.swing.*;
 
 // estoy usando los mismos nombres que usa el profe btw
 public class LibroFrom extends JFrame {
+    LibroServicio libroServicio;
+    private JLabel panel;
+
+    @Autowired
+    public LibroFrom(LibroServicio libSer) {
+        this.libroServicio = libSer;
+        iniciarForma();
+    }
+
+    private void iniciarForma() {
+        setContentPane(panel);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        setSize(900,700);
+    }
 }
