9ac6f9b10e247caed4396f0c1447f24833bf71a2
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-10-06 22:46:21 -0300

    Node - 8.5 Últimas pruebas -> aclaramos conceptos, variables compartidas y error

--- Node/02-Fundamentos_node/async/callback.js
+++ Node/02-Fundamentos_node/async/callback.js
@@ -1,22 +1,25 @@
 function hola(nom, miCallback) {
     setTimeout( function() {
         console.log("Hola " + nom);
-        miCallback();
+        miCallback(nom);
     }, 1000);
 }
 
 function adios(nom, otroCallback) {
     setTimeout( function() {
-        console.log("Adios", nom);
+        console.log("Adios " + nom);
         otroCallback();
-    })
+    }, 1500);
 }
 
 console.log("Iniciando el programa");
 
-hola("carlos", function() {
+hola("carlos", function(nombre) {
 
-    adios("carlos", function() {
+    adios(nombre, function() {
         console.log("Finalizando el programa");
     });
 });
+
+//hola("Carlos", function() {})
+//adios("Carlos", function() {})
