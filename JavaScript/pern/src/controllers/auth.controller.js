5ca0abba67b0bf656f3abfe1d023a73bdfb873a8
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-10-11 00:08:31 -0300

    JS - 8.3 Relacionamos las tareas con el usuario

--- JavaScript/pern/src/controllers/auth.controller.js
+++ JavaScript/pern/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { pool } from "../db.js";
 import bcrypt from "bcrypt";
 import { createAccessToken } from "../libs/jwt.js";
+import md5 from "md5";
 
 export const signin = async (req, res) => {
     const {email, password} = req.body;
@@ -33,7 +34,10 @@ export const signup = async (req, res) => {
 
     try {
         const hashedPassword = await bcrypt.hash(password, 10);
-        const result = await pool.query("INSERT INTO usuarios (name, email, password) VALUES ($1, $2, $3) RETURNING *", [name, email, hashedPassword]);
+
+        const gravatar = `https://gravatar.com/avatar/${md5(email)}`;
+
+        const result = await pool.query("INSERT INTO usuarios (name, email, password, gravatar) VALUES ($1, $2, $3, $4) RETURNING *", [name, email, hashedPassword, gravatar]);
 
         const token = await createAccessToken({id: result.rows[0].id});
         console.log(result);
