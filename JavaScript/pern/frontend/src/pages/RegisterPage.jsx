03715552e13c0ff0acc6dfcf0d3c65c1dd698447
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-10-12 12:42:35 -0300

    JS - 8.5 Formulario de registro - Parte -> 1

--- JavaScript/pern/frontend/src/pages/RegisterPage.jsx
+++ JavaScript/pern/frontend/src/pages/RegisterPage.jsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import Card from '../components/ui/card.jsx';
+import Input from '../components/ui/input.jsx';
+import Button from '../components/ui/button.jsx'
 
 function RegisterPage() {
     return (
-        <h3 className='text-2x1 font-bold text-yellow-300'>Registro</h3>
+        <div className="h-[clac(100vh-64px)] flex items-center justify-center">
+            <Card>
+                <h3 className='text-2x1 font-bold'>Registro</h3>
+                <form >
+                    <Input placeholder="Ingrese su nombre"/>
+                    <Input type="email" placeholder="Ingrese su email"/>
+                    <Input type="password" placeholder="Ingrese su contraseña"/>
+
+                    <Button>Registrarse</Button>
+                </form>
+            </Card>
+        </div>
     )
 }
 
