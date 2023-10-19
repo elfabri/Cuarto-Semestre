56f8c4f6c488e5c6a8c3a1209c2000e7bfb39b45
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-10-19 16:09:43 -0300

    Node - 10.3 promise en adios

--- Node/02-Fundamentos_node/async/promises.js
+++ Node/02-Fundamentos_node/async/promises.js
@@ -14,15 +14,19 @@ function hablar(callbackHablar) {
     }, 1000);
 }
 
-function adios(nom, otroCallback) {
-    setTimeout( function() {
-        console.log("Adios " + nom);
-        otroCallback();
-    }, 1500);
+function adios(nom) {
+    return new Promise((res, rej) => {
+        setTimeout( function() {
+            console.log("Adios " + nom);
+            res();
+        }, 1500);
+    })
 }
 
+// ejejej
 console.log("iniciando proceso");
 hola("Ariel")
+    .then(adios)
     .then((nombre) => {
         console.log("Terminando proceso");
     })
